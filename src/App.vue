f150fa319b0c91a7dc615c4be1dbe937874d1d6b
yuanjiang <cqwkkj@gmail.com>
2023-05-03 18:39:24 +0800

    chore(other): 相关修改

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import 'dayjs/locale/zh-cn';
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/vue.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
 
     <div class="wrapper">
       <nav>
